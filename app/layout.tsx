c47228ac62c48a4debbea80be12f49c90f55b267
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2024-03-11 19:01:09 +0500

    analytics deploy fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
         <Navbar />
         <ProfilePic />
         {children}
+        <Analytics />
       </body>
-      <Analytics />
     </html>
   );
 }
